0e4990a10595f99c7d8afd63c4245e4f10571354
bruno-bouvet <bouvet.bruno@gmail.com>
2016-09-29 16:18:34 +0200

    footer v.1

---
+++ bruno_footer.php
@@ -0,0 +1,44 @@
+
+
+<footer>
+		<div class="footer wrapper">
+			<div class="footer-left">
+				<h3>
+				Qui sommes nous ?
+				</h3>
+				<p> Le Haras de la Métamorphose</p>
+				<p> est un lieux sauvegardé au sein</p>
+				<p> de la forêt de Compiègne ou vous</p> 
+				<p> pourrez retourver toutes vos</p> 
+				<p> activitées chevalines. </p>
+				
+			</div>
+			<div class="footer-center">
+				<h3>Nos coordonnées</h3>
+				<p>110 rue du Colonel Fabien</p>
+				<p>92160 Antony</p>
+				<p id="telephone">Téléphone</p>
+				<p>0146746507</p>
+				
+			</div>
+			<div class="footer-rigth">
+				<h3>
+				Newsletter
+				</h3>
+				<form action="#" method="post">
+
+					<input type="text" name="email" placeholder="Abonnez-vous" />
+
+				</form>
+				<div class="button button1">Abonnez-vous !</div>
+				
+			</div>			
+		</div>
+		<div id="footer-bottom">
+			<div id="copyright">
+				<a href="">© 2016 Haras de la Métamorphose</a>
+<!--				<a id="mentions" href="">Mentions légales</a> -->
+			</div>
+		</div>
+
+	</footer>
