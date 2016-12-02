7047820652ebe4d5de0d7060049d568858279c81
acrobat22 <acrobat22@gmail.com>
2016-12-02 11:44:15 +0100

    CRUD sur POST

---
+++ src/PlateFormeBundle/Form/PostType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace PlateFormeBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('titre')->add('contenu')->add('actif')->add('parent')->add('categorie')->add('user')        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'PlateFormeBundle\Entity\Post'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'plateformebundle_post';
+    }
+
+
+}
