48ddbefbb3048d05856c43994abd54b9b9fade41
AF1890 <aylin.devillechenous@gmail.com>
2016-12-02 12:10:16 +0100

    création CRUD table user + deplacement des vues dans le bundle user

---
+++ src/UserBundle/Form/UserType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('nom')->add('prenom')->add('profession')->add('nbPost')->add('photo')        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'UserBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'userbundle_user';
+    }
+
+
+}
