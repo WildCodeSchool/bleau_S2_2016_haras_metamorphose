6ef786ee5493a1f64e65675cdbe384718df3131a
AF1890 <aylin.devillechenous@gmail.com>
2016-12-06 15:59:01 +0100

    mise en place registration - test

--- src/UserBundle/Form/RegistrationType.php
+++ src/UserBundle/Form/RegistrationType.php
@@ -1,28 +1,22 @@
 <?php
 // src/AppBundle/Form/RegistrationType.php
-
-namespace UserBundle\Form;
-
+namespace UserBundle\Form\;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-
 class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('nom');
     }
-
     public function getParent()
     {
         return 'FOS\UserBundle\Form\Type\RegistrationFormType';
     }
-
     public function getBlockPrefix()
     {
         return 'app_user_registration';
     }
-
     public function getName()
     {
         return $this->getBlockPrefix();
