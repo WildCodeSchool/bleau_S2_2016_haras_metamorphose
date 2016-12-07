df1fed53468a280aa8ad5c4c3a7a68b5011f1068
acrobat22 <acrobat22@gmail.com>
2016-12-07 14:29:44 +0100

    Index post affichage cat et sscat à partir table categoriePlateforme OK

--- src/HarasBundle/Resources/public/scripts/plateforme.js
+++ src/HarasBundle/Resources/public/scripts/plateforme.js
@@ -1,6 +1,10 @@
 /**
  * Created by acrobat on 02/12/2016.
  */
+
+//***************************************************//
+//    Materialize js pour accordeon post index       //
+//*****************************************************
 $(document).ready(function(){
     $('.collapsible').collapsible();
   });
@@ -20,3 +24,8 @@ $('select').material_select('destroy');
 
 // Materialize js pour bouton radio
 $('#mySwitch').prop('checked')
+
+// Gestion aside post index
+$('#expand').click(function(){
+    $('aside').toggle(400);
+});
