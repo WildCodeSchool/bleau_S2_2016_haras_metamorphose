5e9d9790390d98efe8524ac013271e7864f8ac16
acrobat22 <acrobat22@gmail.com>
2017-01-02 21:49:16 +0100

    changement visuel index catégorie, js accordeon ok

--- src/ForumBundle/Resources/public/js/main_forum.js
+++ src/ForumBundle/Resources/public/js/main_forum.js
@@ -60,9 +60,6 @@ $(document).ready(function() {
 // Taille image dans div lastPostContenu
 //****************************************************
 $(document).ready( function () { 
-
-    // $('.lastPostContenu > p > img').css('display','none'); 
-
     // Nb d'image dans le contenu :
     var nbImg = $('.lastPostContenu > p > img').length;
     console.log(nbImg);
@@ -76,19 +73,37 @@ $(document).ready( function () { 
     }); 
 });
 
-
 //***************************************************//
-//    index categorie                                //
+//    index categorie - Accodeon                     //
 //****************************************************
-$(function () {
-    if ($('#toggleLink').attr('rel') == 'hidden') {
-        $('.trSousCat').hide();
-    }
-    else {
-        $('.trSousCat').show();
-    }
-    $('.tdCat a').on('click', function (evt) {
-        evt.preventDefault();
-        $('.trSousCat').toggle();
-    });
+$(document).ready(function() {
+	//ACCORDION BUTTON ACTION (ON CLICK DO THE FOLLOWING)
+	$('.accordionButton').click(function() {
+		//REMOVE THE ON CLASS FROM ALL BUTTONS
+		$('.accordionButton').removeClass('on');
+		//NO MATTER WHAT WE CLOSE ALL OPEN SLIDES
+	 	$('.accordionContent').slideUp('normal');
+		//IF THE NEXT SLIDE WASN'T OPEN THEN OPEN IT
+		if($(this).next().is(':hidden') == true) {
+			//ADD THE ON CLASS TO THE BUTTON
+			$(this).addClass('on');
+			//OPEN THE SLIDE
+			$(this).next().slideDown('normal');
+		 }
+	 });
+
+	/*** REMOVE IF MOUSEOVER IS NOT REQUIRED ***/
+	//ADDS THE .OVER CLASS FROM THE STYLESHEET ON MOUSEOVER
+	$('.accordionButton').mouseover(function() {
+		$(this).addClass('over');
+
+	//ON MOUSEOUT REMOVE THE OVER CLASS
+	}).mouseout(function() {
+		$(this).removeClass('over');
+	});
+	/*** END REMOVE IF MOUSEOVER IS NOT REQUIRED ***/
+	/********************************************************************************************************************
+	CLOSES ALL S ON PAGE LOAD
+	********************************************************************************************************************/
+	$('.accordionContent').hide();
 });
