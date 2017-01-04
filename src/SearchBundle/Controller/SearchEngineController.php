bf88ce7fe123a4559e15d26da523efc95e4b9728
juliengrima <gogrinformatique@gmail.com>
2017-01-04 17:51:08 +0100

    search controller a tester

--- src/SearchBundle/Controller/SearchEngineController.php
+++ src/SearchBundle/Controller/SearchEngineController.php
@@ -22,73 +22,46 @@ class SearchEngineController extends Controller
         {
             $bdd= new PDO('mysql:host=localhost;dbname=haras;charset=utf8', 'root', 'root');
 
-            // on crée une variable $requete pour faciliter l'écriture de la requête SQL, mais aussi pour empêcher les éventuels malins qui utiliseraient du PHP ou du JS, avec la fonction htmlspecialchars().
+            // on crée une variable $requete pour faciliter l'écriture de la requête SQL,
+            // mais aussi pour empêcher les éventuels malins qui utiliseraient du PHP ou du JS,
+            // avec la fonction htmlspecialchars().
             $requete = htmlspecialchars($_POST['requete']);
 
-            // la requête, que vous devez maintenant comprendre ;)
-            $query = $bdd->prepare("SELECT * FROM fonctions WHERE nom_fonction LIKE '%$requete%' ORDER BY id DESC") or die ();
+            // la requête, que vous devez maintenant comprendre et modifier;)
+            $query = $bdd->prepare("SELECT * FROM post, categorie_plateforme WHERE post.titre, post.contenu, categorie_plateforme.nom  LIKE '%$requete%' ORDER BY id DESC") or die ();
 
-            $nb_resultats = mysql_num_rows($query); // on utilise la fonction mysql_num_rows pour compter les résultats pour vérifier par après
+            // on utilise la fonction mysql_num_rows pour compter les résultats pour vérifier par après
+            $nb_resultats = mysql_num_rows($query);
 
-            if($nb_resultats != 0) // si le nombre de résultats est supérieur à 0, on continue
+            // si le nombre de résultats est supérieur à 0, on continue
+            if($nb_resultats != 0)
 
             {
 
-// maintenant, on va afficher les résultats et la page qui les donne ainsi que leur nombre, avec un peu de code HTML pour faciliter la tâche.
+                // maintenant, on va afficher les résultats et la page qui les donne ainsi que
+                // leur nombre, avec un peu de code HTML pour faciliter la tâche.
 
-                ?>
+                return $this->render('@Search/Default/index.html.twig', array(
+                    'query' => $query,
+                ));
 
-                <h3>Résultats de votre recherche.</h3>
-
-                <p>Nous avons trouvé <? echo $nb_resultats; // on affiche le nombre de résultats
-
-                    if($nb_resultats > 1) { echo 'résultats'; } else { echo 'résultat'; } // on vérifie le nombre de résultats pour orthographier correctement.
-
-                    ?>
-
-                    dans notre base de données. Voici les fonctions que nous avons trouvées :<br/>
-
-                    <br/>
-
-                    <?
-
-                    while($donnees = mysql_fetch_array($query)) // on fait un while pour afficher la liste des fonctions trouvées, ainsi que l'id qui permettra de faire le lien vers la page de la fonction
-
-                    {
-
-                        ?>
-
-                        <a href="fonction.php?id=<? echo $donnees['id']; ?>"><? echo $donnees['nom_fonction']; ?></a><br/>
-
-                        <?
-
-                    } // fin de la boucle
-
-                    ?><br/>
-
-                    <br/>
-
-                    <a href="rechercher.php">Faire une nouvelle recherche</a></p>
-
-                <?
-
-            } // Fini d'afficher les résultats ! Maintenant, nous allons afficher l'éventuelle erreur en cas d'échec de recherche et le formulaire.
+            }
+            // Fini d'afficher les résultats ! Maintenant, nous allons afficher
+            // l'éventuelle erreur en cas d'échec de recherche et le formulaire.
 
             else
 
             { // de nouveau, un peu de HTML
 
-                ?>
+                return $this->render('@PLateForme/Default/index.html.twig');
 
-                <h3>Pas de résultats</h3>
-
-                <p>Nous n'avons trouvé aucun résultat pour votre requête "<? echo $_POST['requete']; ?>". <a href="rechercher.php">Réessayez</a> avec autre chose.</p>
-
-                <?
+                $this->addFlash(
+                    'success',
+                    'La recherche ne donne aucun résultats'
+                );
 
             }// Fini d'afficher l'erreur ^^
 
-            mysql_close(); // on ferme mysql, on n'en a plus besoin
 
         }
 
@@ -96,19 +69,12 @@ class SearchEngineController extends Controller
 
         { // et voilà le formulaire, en HTML de nouveau !
 
-            ?>
+            return $this->render('@PLateForme/Default/index.html.twig');
 
-            <p>Vous allez faire une recherche dans notre base de données concernant les fonctions PHP. Tapez une requête pour réaliser une recherche.</p>
-
-            <form action="rechercher.php" method="Post">
-
-                <input type="text" name="requete" size="10">
-
-                <input type="submit" value="Ok">
-
-            </form>
-
-            <?
+            $this->addFlash(
+                'success',
+                'La recherche ne donne aucun résultats'
+            );
 
         }
 
