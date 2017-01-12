990bcbf9ada04baac1b1e4f90befd00d860d4a69
AF1890 <aylin.devillechenous@gmail.com>
2017-01-12 18:11:35 +0100

    ajout upload image pour le register

--- src/UserBundle/Controller/RegistrationController.php
+++ src/UserBundle/Controller/RegistrationController.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * This file is part of the FOSUserBundle package.
  *
@@ -8,9 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace UserBundle\Controller;
-
 use FOS\UserBundle\Event\FilterUserResponseEvent;
 use FOS\UserBundle\Event\FormEvent;
 use FOS\UserBundle\Event\GetResponseUserEvent;
@@ -25,7 +22,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-
 /**
  * Controller managing the registration.
  *
@@ -47,79 +43,68 @@ class RegistrationController extends Controller
         $userManager = $this->get('fos_user.user_manager');
         /** @var $dispatcher EventDispatcherInterface */
         $dispatcher = $this->get('event_dispatcher');
-
         $user = $userManager->createUser();
         $user->setEnabled(true);
-
         $event = new GetResponseUserEvent($user, $request);
         $dispatcher->dispatch(FOSUserEvents::REGISTRATION_INITIALIZE, $event);
-
         if (null !== $event->getResponse()) {
             return $event->getResponse();
         }
-
         $form = $formFactory->createForm();
         $form->setData($user);
-
         $form->handleRequest($request);
-
         if ($form->isSubmitted()) {
-
             if ($form->isValid()) {
                 $event = new FormEvent($form, $request);
                 $dispatcher->dispatch(FOSUserEvents::REGISTRATION_SUCCESS, $event);
 
-                $userManager->updateUser($user);
+                $mediaForm = $form->get('photo');
+                $media = $mediaForm->getData();
+                $user->setPhoto($media);
+                $media->setName(preg_replace('/\W/', '_', $user->getNom()) );
+                // L'alt définit son nom selon celui de son médium et prend un '_' juste pour rester dans l'ambiance \(o°v°o)/
+                $media->getAlt()->setName($media->getName() . "_Alt");
+                // On appelle le service d'upload de média (HarasBundle/Services/mediaInterface)
+                $this->get('media.interface')->mediaUpload($media);
 
+                $userManager->updateUser($user);
                 if (null === $response = $event->getResponse()) {
                     $url = $this->getParameter('fos_user.registration.confirmation.enabled')
                         ? $this->generateUrl('fos_user_registration_confirmed')
                         : $this->generateUrl('fos_user_profile_show');
-
                     $response = new RedirectResponse($url);
                 }
-
                 $dispatcher->dispatch(FOSUserEvents::REGISTRATION_COMPLETED, new FilterUserResponseEvent($user, $request, $response));
-
                 return $response;
             }
-
             $event = new FormEvent($form, $request);
             $dispatcher->dispatch(FOSUserEvents::REGISTRATION_FAILURE, $event);
-
             if (null !== $response = $event->getResponse()) {
                 return $response;
             }
         }
-
         return $this->render('FOSUserBundle:Registration:register.html.twig', array(
             'form' => $form->createView(),
         ));
     }
-
     /**
      * Tell the user to check his email provider.
      */
     public function checkEmailAction()
     {
         $email = $this->get('session')->get('fos_user_send_confirmation_email/email');
-
         if (empty($email)) {
             return new RedirectResponse($this->get('router')->generate('fos_user_registration_register'));
         }
-
         $this->get('session')->remove('fos_user_send_confirmation_email/email');
         $user = $this->get('fos_user.user_manager')->findUserByEmail($email);
-
         if (null === $user) {
             throw new NotFoundHttpException(sprintf('The user with email "%s" does not exist', $email));
         }
-
         return $this->render('FOSUserBundle/Registration/check_email.html.twig', array(
             'user' => $user,
         ));
     }
-
     /**
      * Receive the confirmation token from user email provider, login the user.
      *
@@ -132,34 +117,24 @@ class RegistrationController extends Controller
     {
         /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
         $userManager = $this->get('fos_user.user_manager');
-
         $user = $userManager->findUserByConfirmationToken($token);
-
         if (null === $user) {
             throw new NotFoundHttpException(sprintf('The user with confirmation token "%s" does not exist', $token));
         }
-
         /** @var $dispatcher EventDispatcherInterface */
         $dispatcher = $this->get('event_dispatcher');
-
         $user->setConfirmationToken(null);
         $user->setEnabled(true);
-
         $event = new GetResponseUserEvent($user, $request);
         $dispatcher->dispatch(FOSUserEvents::REGISTRATION_CONFIRM, $event);
-
         $userManager->updateUser($user);
-
         if (null === $response = $event->getResponse()) {
             $url = $this->generateUrl('fos_user_registration_confirmed');
             $response = new RedirectResponse($url);
         }
-
         $dispatcher->dispatch(FOSUserEvents::REGISTRATION_CONFIRMED, new FilterUserResponseEvent($user, $request, $response));
-
         return $response;
     }
-
     /**
      * Tell the user his account is now confirmed.
      */
@@ -169,20 +144,17 @@ class RegistrationController extends Controller
         if (!is_object($user) || !$user instanceof UserInterface) {
             throw new AccessDeniedException('This user does not have access to this section.');
         }
-
         return $this->render('FOSUserBundle:Registration:confirmed.html.twig', array(
             'user' => $user,
             'targetUrl' => $this->getTargetUrlFromSession(),
         ));
     }
-
     /**
      * @return mixed
      */
     private function getTargetUrlFromSession()
     {
         $key = sprintf('_security.%s.target_path', $this->get('security.token_storage')->getToken()->getProviderKey());
-
         if ($this->get('session')->has($key)) {
             return $this->get('session')->get($key);
         }
