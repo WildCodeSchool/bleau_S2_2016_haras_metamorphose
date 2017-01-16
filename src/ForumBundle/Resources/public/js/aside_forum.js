f3d7a9a227b94041fb1534f728a18ac4315e9677
acrobat22 <acrobat22@gmail.com>
2017-01-16 18:05:39 +0100

    ajout jquery pour gestion CSS mobile et tablette

--- src/ForumBundle/Resources/public/js/aside_forum.js
+++ src/ForumBundle/Resources/public/js/aside_forum.js
@@ -1,7 +1,44 @@
 $(document).ready(function() {
-   $('header').css('padding-left', '300px');
-   $('.header-logo').css('margin-left', '72px');
-   $('.hide-on-med-and-down').css('margin-left', '70px');
-   $('.page-footer').css( 'margin-left', '300px');
-   $('.page-footer.container').css('margin-left', '-152px');
+    $(window).resize(function() {
+        if ($(window).width() < 1030) {
+            $('aside').addClass('hide');
+            $('.postSectionIndex').each(function () {
+                this.style.setProperty( 'margin', '10px', 'important' );
+            });
+            $('.liPostSousCat').each(function () {
+                this.style.setProperty( 'margin', '10px', 'important' );
+            });
+            $('.postListTitleSousCat ').css('font-size', '10px');
+            $('.postDivPrincipale ').css('font-size', '10px');
+            $('.badge').css('font-size', '10px');
+            $('.postListTitleSousCat').find( 'i' ).addClass('hide-on-med-and-down');
+            $('.postListTitlePost').find( 'i' ).addClass('hide-on-med-and-down');
+            // $('.postListTitlePost').each(function(){
+            //     $(this).text($(this).text().substring(0,20)+"...");
+            // });
+
+            if ($('.postListTitlePost').length > 20 ) {
+                // trims it to 17 charcters
+                $(this).text($(this).text().substring(0,17));
+
+                // 17 charcters + .......
+                $(this).append('.....');
+            }
+            $('.lastPostContenu').addClass('hide');
+            $('header').css('padding-left', '0');
+            $('wrapperAllPost').css('margin-left', '-12%');
+            $('card-content').css('hide');
+            $('.header-logo').css('margin-left', '0');
+            $('.hide-on-med-and-down').css('margin-left', '0');
+            $('.page-footer').css( 'margin-left', '0');
+            $('.page-footer.container').css('margin-left', '0');
+        }
+        else {
+            $('header').css('padding-left', '300px');
+            $('.header-logo').css('margin-left', '72px');
+            $('.hide-on-med-and-down').css('margin-left', '70px');
+            $('.page-footer').css( 'margin-left', '300px');
+            $('.page-footer.container').css('margin-left', '-152px');
+        }
+    });
 });
