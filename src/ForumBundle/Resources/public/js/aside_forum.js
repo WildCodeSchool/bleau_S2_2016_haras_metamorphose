aaf0a0466cdd673d73947cdba7555a60ad58b5db
acrobat22 <acrobat22@gmail.com>
2017-01-17 16:32:20 +0100

    changement cartouche derniers post homepage + responsive

--- src/ForumBundle/Resources/public/js/aside_forum.js
+++ src/ForumBundle/Resources/public/js/aside_forum.js
@@ -1,15 +1,29 @@
 $(document).ready(function() {
+
+    $('header').css('padding-left', '300px');
+    $('.header-logo').css('margin-left', '72px');
+    $('.hide-on-med-and-down').css('margin-left', '70px');
+    $('.page-footer').css( 'margin-left', '300px');
+    $('.page-footer.container').css('margin-left', '-152px');
+
     $(window).resize(function() {
+
+        $('header').css('padding-left', '300px');
+        $('.header-logo').css('margin-left', '72px');
+        $('.hide-on-med-and-down').css('margin-left', '70px');
+        $('.page-footer').css( 'margin-left', '300px');
+        $('.page-footer.container').css('margin-left', '-152px');
+
         if ($(window).width() < 1000) {
             // $('.postListTitlePost').each(function(){
-//     $(this).text($(this).text().substring(0,20)+"...");
-// });
+            //     $(this).text($(this).text().substring(0,20)+"...");
+            // });
 
             if ($('.postListTitlePost').length > 20 ) {
-// trims it to 17 charcters
+            // trims it to 17 charcters
                 $(this).text($(this).text().substring(0,17));
 
-// 17 charcters + .......
+            // 17 charcters + .......
                 $(this).append('.....');
             }
             $('header').css('padding-left', '0');
@@ -17,13 +31,6 @@ $(document).ready(function() {
             $('.hide-on-med-and-down').css('margin-left', '0');
             $('.page-footer').css( 'margin-left', '0');
             $('.page-footer.container').css('margin-left', '0');
-        }
-        else {
-            $('header').css('padding-left', '300px');
-            $('.header-logo').css('margin-left', '72px');
-            $('.hide-on-med-and-down').css('margin-left', '70px');
-            $('.page-footer').css( 'margin-left', '300px');
-            $('.page-footer.container').css('margin-left', '-152px');
         }
     });
 });
