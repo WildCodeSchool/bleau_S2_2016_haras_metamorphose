14db9d0c35a75cbde650ac2450f562a10f73e1e9
acrobat22 <acrobat22@gmail.com>
2017-01-18 11:32:56 +0100

    factorisation jquery aside

--- src/ForumBundle/Resources/public/js/aside_forum.js
+++ src/ForumBundle/Resources/public/js/aside_forum.js
@@ -1,31 +1,21 @@
 $(document).ready(function() {
 
-    $('header').css('padding-left', '300px');
-    $('.header-logo').css('margin-left', '72px');
-    $('.hide-on-med-and-down').css('margin-left', '70px');
-    $('.page-footer').css( 'margin-left', '300px');
-    $('.page-footer.container').css('margin-left', '-152px');
+    // Modification taille header général et footer
+    modifHeaderFooter ();
 
     $(window).resize(function() {
 
-        $('header').css('padding-left', '300px');
-        $('.header-logo').css('margin-left', '72px');
-        $('.hide-on-med-and-down').css('margin-left', '70px');
-        $('.page-footer').css( 'margin-left', '300px');
-        $('.page-footer.container').css('margin-left', '-152px');
+        modifHeaderFooter ();
 
         if ($(window).width() < 1000) {
-            // $('.postListTitlePost').each(function(){
-            //     $(this).text($(this).text().substring(0,20)+"...");
-            // });
-
             if ($('.postListTitlePost').length > 20 ) {
-            // trims it to 17 charcters
+            // Récupération des 17 premiers caractères
                 $(this).text($(this).text().substring(0,17));
-
-            // 17 charcters + .......
+            // 17 caractères + .......
                 $(this).append('.....');
             }
+
+            // Modification taille header général et footer
             $('header').css('padding-left', '0');
             $('.header-logo').css('margin-left', '0');
             $('.hide-on-med-and-down').css('margin-left', '0');
@@ -34,3 +24,11 @@ $(document).ready(function() {
         }
     });
 });
+
+function modifHeaderFooter () {
+    $('header').css('padding-left', '300px');
+    $('.header-logo').css('margin-left', '72px');
+    $('.hide-on-med-and-down').css('margin-left', '70px');
+    $('.page-footer').css( 'margin-left', '300px');
+    $('.page-footer.container').css('margin-left', '-152px');
+}
