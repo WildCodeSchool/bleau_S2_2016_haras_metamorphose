2231b68c7a6c3f339eca18f1fe01d6ab784ebecb
AF1890 <aylin.devillechenous@gmail.com>
2017-01-18 14:51:25 +0100

    security moderateur

--- src/UserBundle/Controller/UserController.php
+++ src/UserBundle/Controller/UserController.php
@@ -66,7 +66,7 @@ class UserController extends Controller
         if ($editForm->isSubmitted() && $editForm->isValid()) {
             $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('user_edit', array('id' => $user->getId()));
+            return $this->redirectToRoute('user_show', array('id' => $user->getId()));
         }
 
         return $this->render('@User/user/edit.html.twig', array(
