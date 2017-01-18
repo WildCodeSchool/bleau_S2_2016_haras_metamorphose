f21aadfb18ed12c7100f4ef88baca60fabaedb05
juliengrima <gogrinformatique@gmail.com>
2017-01-18 14:03:49 +0100

    reprise du moteur de recherche

--- src/SearchBundle/Controller/SearchEngineController.php
+++ src/SearchBundle/Controller/SearchEngineController.php
@@ -9,10 +9,70 @@
 namespace SearchBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use ForumBundle\Entity\Post;
+use ForumBundle\Entity\CategoriePlateforme;
+use Symfony\Component\HttpFoundation\Request;
+use SearchBundle\Repository\SearchRepository;
 
 class SearchEngineController extends Controller
 {
+    // METHODE DE RECHERCHE PAR MOTS
+    public function searchAction(Request $request, Entity\Post $post, Entity\CategoriePlateforme $categoriePlateforme)
+    {
+        // on vérifie d'abord l'existence du POST et aussi si la requete n'est pas vide.
+        if(isset($_POST['requete']) && $_POST['requete'] != NULL)
 
+        {
+            // on crée une variable $requete pour faciliter l'écriture de la requête SQL,
+            // mais aussi pour empêcher les éventuels malins qui utiliseraient du PHP ou du JS,
+            // avec la fonction htmlspecialchars().
+            $requete = htmlspecialchars($_POST['requete']);
+
+
+            // Appel du repository avec laquelle on demande une requete sql
+            $repository = $this->getDoctrine()->getManager()->getRepository(SearchRepository::class);
+
+            // on boucle pour récuperer le résultat de repository
+            foreach ($repository as $resultats) {
+
+                $resultat = $repository->findBy($requete);
+
+            }
+
+
+            // si le nombre de résultats est supérieur à 0, renvoie un résultat
+            if($resultat != 0)
+
+            {
+                // maintenant, on va afficher les résultats et la page qui les donne ainsi que
+                // leur nombre, avec un peu de code HTML pour faciliter la tâche.
+                return $this->render('@Search/Default/index.html.twig', array(
+                    'resultat' => $resultat,
+                ));
+            }
+            // sinon on retourne à la page d'accueil avec un message
+            else
+
+            {
+//                $this->addFlash(
+//                    'success',
+//                    'La recherche ne donne aucun résultats'
+//                );
+
+                return $this->render('@PlateForme/Default/index.html.twig');
+
+            }
+        }
+        // Si le post est vide on retourne à la page d'accueil
+        else
+        {
+//            $this->addFlash(
+//                'success',
+//                'Le champ de recherche est vide'
+//            );
+
+            return $this->render('@PlateForme/Default/index.html.twig');
+        }
+    }
 
 }
--- src/SearchBundle/Repository/SearchRepository.php
+++ src/SearchBundle/Repository/SearchRepository.php
@@ -3,6 +3,7 @@
 namespace SearchBundle\Repository;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Doctrine\ORM\EntityRepository;
 
 /**
  * AgendaRepository
@@ -12,78 +13,109 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
  */
 //use Doctrine\ORM\Mapping;
 
-class SearchRepository extends \Doctrine\ORM\EntityRepository
+class SearchRepository extends EntityRepository
 {
+    public function findPosts($limit, $requete)
+    {
+//        Alias 's' = class searchrepository
+//        Alias 'p' = jointure parent_id
 
-    public function searchAction(){
 
-        // on vérifie d'abord l'existence du POST et aussi si la requete n'est pas vide.
-        if(isset($_POST['requete']) && $_POST['requete'] != NULL)
-
-        {
-            $bdd= new PDO('mysql:host=localhost;dbname=haras;charset=utf8', 'root', 'root');
-//            $bdd = $this->getDoctrine()->getManager();
-
-            // on crée une variable $requete pour faciliter l'écriture de la requête SQL,
-            // mais aussi pour empêcher les éventuels malins qui utiliseraient du PHP ou du JS,
-            // avec la fonction htmlspecialchars().
-            $requete = htmlspecialchars($_POST['requete']);
-
-            // la requête, que vous devez maintenant comprendre et modifier;)
-            $query = $bdd->prepare("SELECT titre, contenu, nom 
-                                    FROM post, categorie_plateforme 
-                                    WHERE post.parent_id = categorie_plateforme.parent_id 
-                                    AND contenu = $requete 
-                                    OR titre = $requete 
-                                    OR nom = $requete
-                                    ORDER BY post.id DESC") OR DIE ();
-
-            //on execute la requete
-            $resultat = $query->execute(array(
-                'requete' => $requete
-            ));
-
-            $donnees = $requete->fetch($query);
-
-            // on utilise la fonction mysql_num_rows pour compter les résultats pour vérifier
-//            $nb_resultats = count($result);
-
-            // si le nombre de résultats est supérieur à 0, renvoie un résultat
-            if($donnees != "")
-
-            {
-                // maintenant, on va afficher les résultats et la page qui les donne ainsi que
-                // leur nombre, avec un peu de code HTML pour faciliter la tâche.
-                return $this->render('@Search/Default/index.html.twig', array(
-                    'query' => $donnees,
-                    'nb_resultats' => $donnees,
-                ));
-            }
-            // sinon on retourne à la page d'accueil avec un message
-            else
-
-            {
-//                $this->addFlash(
-//                    'success',
-//                    'La recherche ne donne aucun résultats'
-//                );
-
-                return $this->render('@PlateForme/Default/index.html.twig');
-
-            }
-        }
-        // Si le post est vide on retourne à la page d'accueil
-        else
-        {
-//            $this->addFlash(
-//                'success',
-//                'Le champ de recherche est vide'
-//            );
-
-            return $this->render('@PlateForme/Default/index.html.twig', array(
-                'query' => $post
-            ));
-        }
-
+        $qb = $this->createQueryBuilder('s')
+            ->select('s.post')
+            ->orderBy('s.date_create', 'DESC')
+            ->setMaxResults( $limit );
+        return $qb->getQuery()->getResult();
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    public function searchAction(){
+//
+//        // on vérifie d'abord l'existence du POST et aussi si la requete n'est pas vide.
+//        if(isset($_POST['requete']) && $_POST['requete'] != NULL)
+//
+//        {
+//            $bdd= new PDO('mysql:host=localhost;dbname=haras;charset=utf8', 'root', 'root');
+////            $bdd = $this->getDoctrine()->getManager();
+//
+//            // on crée une variable $requete pour faciliter l'écriture de la requête SQL,
+//            // mais aussi pour empêcher les éventuels malins qui utiliseraient du PHP ou du JS,
+//            // avec la fonction htmlspecialchars().
+//            $requete = htmlspecialchars($_POST['requete']);
+//
+//            // la requête, que vous devez maintenant comprendre et modifier;)
+//            $query = $bdd->prepare("SELECT titre, contenu, nom
+//                                    FROM post, categorie_plateforme
+//                                    WHERE post.parent_id = categorie_plateforme.parent_id
+//                                    AND contenu = $requete
+//                                    OR titre = $requete
+//                                    OR nom = $requete
+//                                    ORDER BY post.id DESC") OR DIE ();
+//
+//            //on execute la requete
+//            $resultat = $query->execute(array(
+//                'requete' => $requete
+//            ));
+//
+//            $donnees = $requete->fetch($query);
+//
+//            // on utilise la fonction mysql_num_rows pour compter les résultats pour vérifier
+////            $nb_resultats = count($result);
+//
+//            // si le nombre de résultats est supérieur à 0, renvoie un résultat
+//            if($donnees != "")
+//
+//            {
+//                // maintenant, on va afficher les résultats et la page qui les donne ainsi que
+//                // leur nombre, avec un peu de code HTML pour faciliter la tâche.
+//                return $this->render('@Search/Default/index.html.twig', array(
+//                    'query' => $donnees,
+//                    'nb_resultats' => $donnees,
+//                ));
+//            }
+//            // sinon on retourne à la page d'accueil avec un message
+//            else
+//
+//            {
+////                $this->addFlash(
+////                    'success',
+////                    'La recherche ne donne aucun résultats'
+////                );
+//
+//                return $this->render('@PlateForme/Default/index.html.twig');
+//
+//            }
+//        }
+//        // Si le post est vide on retourne à la page d'accueil
+//        else
+//        {
+////            $this->addFlash(
+////                'success',
+////                'Le champ de recherche est vide'
+////            );
+//
+//            return $this->render('@PlateForme/Default/index.html.twig', array(
+//                'query' => $post
+//            ));
+//        }
+//
+//    }
 }
