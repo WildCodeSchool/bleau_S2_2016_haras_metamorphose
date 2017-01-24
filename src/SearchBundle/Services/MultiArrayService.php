f72cc3691e77d6350af4937e216542d1e0b5ed13
juliengrima <gogrinformatique@gmail.com>
2017-01-24 18:00:10 +0100

    d

--- src/SearchBundle/Services/MultiArrayService.php
+++ src/SearchBundle/Services/MultiArrayService.php
@@ -23,32 +23,32 @@ class MultiArrayService extends Controller
     // Fonction pour lire $repository en tant que tableau multi-dimensionnel
     public function multiArray($titres, $requete){
 
-        $result = array();
         foreach ($titres as $key => $titre){
 
             /*--- Si $titre correspond a la requete de départ il check uniquement le 1er tableau---*/
             if (in_array($requete, $titre)){
 
+                foreach ($titre as $value){
+
+                    dump($value); die;
+
+
+
+                    preg_match($requete, $value, $result);
+
+                    $result_titre = $this->getDoctrine()->getRepository('ForumBundle:Post')->findAll($result);
+                }
+
+
+
                     $result['titre'] = $titre['titre'];
+
+
 
                 return $titre;
             }
-            /*--- Sinon on boucle sur le 2nd tableau et on check la valeur de retour est egale à la requete---*/
-            else{
 
-                $requete_str = '/' . $requete . '/';
 
-                foreach ($titres as $valeur){
-
-                    if (preg_match($requete_str, $valeur['contenu'], $resultat)){
-
-                        $result['contenu'] = $valeur;
-
-                        // on renvois la valeur du titre en fonction du mot trouvé dans le contenu
-                        return $valeur;
-                    }
-                }
-            }
         }
     }
 
