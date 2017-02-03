64891e7201f11f3daed294c2b6ad3f747afb00f7
Florian Grandjean <florian.pdf@gmail.com>
2017-02-03 12:24:25 +0100

    test csv export

--- src/PlateFormeBundle/Controller/DefaultController.php
+++ src/PlateFormeBundle/Controller/DefaultController.php
@@ -1,6 +1,7 @@
 <?php
 namespace PlateFormeBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 use UserBundle\Entity\User;
 
@@ -71,7 +72,103 @@ class DefaultController extends Controller
      */
     public function generateCsvAction() {
         $response= new StreamedResponse();
-        $users = $this->getDoctrine()->getRepository('UserBundle:User')->findAll();
+        $users = $this->getDoctrine()->getRepository('UserBundle\Entity\User')->findAll();
+        $response->setCallback(function() use ($users) {
+            $handle = fopen('php://output','w+');
+            fputcsv($handle, array('id', 'Nom', 'Prenom','Nom Utilisateur', 'Email', 'Profession', 'Actif', 'Newsletter'),';');
+            //$results = $this->connection->query('SELECT email, gender, country, city, tshirt, foodPreference FROM user');
+            //$results->execute();
+            foreach ($users as $user) {
+
+                if($user->getActif() == 1) {
+                    $userActif = "oui";
+                } else {
+                    $userActif = "non";
+                }
+
+                if($user->getNewsletter() == 1) {
+                    $userNL = "oui";
+                } else {
+                    $userNL = "non";
+                }
+
+                fputcsv($handle, array(
+                    $user->getId(),
+                    $user->getNom(),
+                    $user->getPrenom(),
+                    $user->getUsername(),
+                    $user->getEmail(),
+                    $user->getProfession(),
+                    $userActif,
+                    $userNL
+                ),';');
+
+                $userActif = "";
+                $userNL = "";
+            }
+            fclose($handle);
+        });
+        $response->setStatusCode(200);
+        $response->headers->set('Content-Type', 'text/csv; charset=utf-8');
+        $response->headers->set('Content-Disposition','attachment; filename="listeDesUtilisateurs.csv"');
+        return $response;
+    }
+
+
+#///////////////////////////////////////////////////////////////////////////
+#///////////////////////////////////////////////////////////////////////////
+#TEST EXPORT CSV
+#///////////////////////////////////////////////////////////////////////////
+#///////////////////////////////////////////////////////////////////////////
+
+    public function showTablesAction(){
+        $em = $this->getDoctrine()->getManager();
+        $entities = array();
+        $meta = $em->getMetadataFactory()->getAllMetadata();
+        foreach ($meta as $m) {
+            $entities[$m->getName()] = preg_replace('/.*\\\\/', '', $m->getName());
+        }
+
+        return $this->render('@PlateForme/showTables.html.twig', array(
+            'entities' => $entities
+        ));
+    }
+
+    public function getTableFieldAction(){
+        $entity = $_POST['table'];
+        $session = $this->get('session');
+        $session->set('linkEntity', $entity);
+        $em = $this->getDoctrine()->getManager();
+        $columnEntity = $em->getClassMetadata($entity)->getFieldNames();
+
+        return $this->render('@PlateForme/showTables.html.twig', array(
+            'columns' => $columnEntity
+        ));
+    }
+
+    /**
+     * Export user en CSV.
+     *
+     */
+    public function generateCsv2Action() {
+        $session = $this->get('session');
+
+        $nameEntity = $_POST['entity'];
+        $linkEntity = $session->get('linkEntity');
+
+        $field = '';
+        $count = count($nameEntity);
+        for ($i=0; $i < $count; $i++){
+            if ($i == $count - 1){
+                $field .= $nameEntity[$i];
+            }
+            else{
+                $field .= $nameEntity[$i] . ', ';
+            }
+        }
+
+        $response= new StreamedResponse();
+        $users = $this->getDoctrine()->getRepository($linkEntity)->findAll();
         $response->setCallback(function() use ($users) {
             $handle = fopen('php://output','w+');
             fputcsv($handle, array('id', 'Nom', 'Prenom','Nom Utilisateur', 'Email', 'Profession', 'Actif', 'Newsletter'),';');
